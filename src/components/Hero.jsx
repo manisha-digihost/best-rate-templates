fe8bc1bb79ead6c79e6c0e6b9e40f2e01c363dff
swapnilrajput26 <swapnil.rajput@digihost.in>
2025-01-20 13:38:09 +0530

    usp and hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,40 +3,31 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function Hero() {
   return (
-    <Carousel data-bs-theme="dark">
+    <Carousel id="myCarousel" className="mb-6">
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://img.freepik.com/free-photo/integrated-microchip-circuit-board-server-central-computer-processor-with-neon-lights_90220-1202.jpg?t=st=1737356235~exp=1737359835~hmac=b41d951e0683c114493903e91e06aff4b08eea0c1b200bf6b901f7ab9f49318e&w=900"
-          alt="First slide"
-        />
+        <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
         <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h1>Example headline.</h1>
+          <p className="opacity-75">Some representative placeholder content for the first slide of the carousel.</p>
+          <p><a className="btn btn-lg btn-primary" href="#">Sign up today</a></p>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://img.freepik.com/free-photo/integrated-microchip-circuit-board-server-central-computer-processor-with-neon-lights_90220-1202.jpg?t=st=1737356235~exp=1737359835~hmac=b41d951e0683c114493903e91e06aff4b08eea0c1b200bf6b901f7ab9f49318e&w=900"
-          alt="Second slide"
-        />
+        <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
         <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h1>Another example headline.</h1>
+          <p>Some representative placeholder content for the second slide of the carousel.</p>
+          <p><a className="btn btn-lg btn-primary" href="#">Learn more</a></p>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://img.freepik.com/free-photo/integrated-microchip-circuit-board-server-central-computer-processor-with-neon-lights_90220-1202.jpg?t=st=1737356235~exp=1737359835~hmac=b41d951e0683c114493903e91e06aff4b08eea0c1b200bf6b901f7ab9f49318e&w=900"
-          alt="Third slide"
-        />
+        <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
         <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h1>One more for good measure.</h1>
+          <p>Some representative placeholder content for the third slide of this carousel.</p>
+          <p><a className="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
