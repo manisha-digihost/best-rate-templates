1fb3758c756d918bfb8faab71dff785499d6666e
Devendra Mali <devendra.mali@digihost.in>
2025-01-20 17:24:39 +0530

    new chnages

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Testimonials = () => {
+  const settings = {
+    centerMode: true,
+    centerPadding: "40px",
+    dots: true,
+    slidesToShow: 3,
+    infinite: true,
+    arrows: false,
+    lazyLoad: "ondemand",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  };
+
   return (
-    <div>
-      <h2>Testimonials</h2>
+    <div className="testimonial">
+      <div className="heading">
+        <Row  className="justify-content-center">
+          <Col lg={4}>
+          <h2>Testimonial</h2>
+          </Col>
+        </Row>
+      </div>
+      <Slider {...settings}>
+        {[1, 2, 3, 4].map((index) => (
+          <div key={index} className="testimonial-item">
+            <div className="box">
+              <figure className="image">
+               <div className="img"></div>
+              </figure>
+              <div className="test-component">
+                  <h4>Efecha Omoware</h4>
+                  <p>I now enjoy financial freedom like I’ve never known before doing what I love.I now enjoy financial freedom like I’ve never known before doing what I love.I now enjoy financial freedom like I’ve never known before doing what I love.</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
