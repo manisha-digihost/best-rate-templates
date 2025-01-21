c05863d99e93a4d6dbc07e76c12fcc8af3947d50
manisha-digihost <manisha.prajapati@digihost.in>
2025-01-21 18:09:56 +0530

    route issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import BaseTemplate2 from "./pages/BaseTemplate2";
 // import Header from "./layout/Hea";
 import Footer from "./layout/Footer/Footer";
 import Header from "./layout/Header/Header";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       {/* <Header /> */}
-      <Routes>
-      <Route Component={BaseTemplate} path="/" element={<BaseTemplate />} />
-      <Route Component={BaseTemplate2} path="/base-template-2" element={<BaseTemplate2 />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route Component={BaseTemplate} path="/" element={<BaseTemplate />} />
+          <Route
+            Component={BaseTemplate2}
+            path="/base-template-2"
+            element={<BaseTemplate2 />}
+          />
+        </Routes>
+      </Router>
       {/* <Footer /> */}
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "../src/assets/scss/style.scss";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter> */}
+    <App />
+    {/* </BrowserRouter> */}
   </StrictMode>
 );
