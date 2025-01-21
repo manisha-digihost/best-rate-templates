87755ab1daf94c8e4effbd2cf0107fa794ef2df0
manisha-digihost <manisha.prajapati@digihost.in>
2025-01-21 18:31:29 +0530

    route issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import BaseTemplate from "./pages/BaseTemplate";
 import BaseTemplate2 from "./pages/BaseTemplate2";
-// import Header from "./layout/Hea";
 import Footer from "./layout/Footer/Footer";
 import Header from "./layout/Header/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        {/* <Header /> */}
-        <Routes>
-          <Route path="/" element={<BaseTemplate />} />
-          <Route path="/base-template-2" element={<BaseTemplate2 />} />
-        </Routes>
-        {/* <Footer /> */}
-      </div>
-    </Router>
+    <div>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<BaseTemplate />} />
+        <Route path="/base-template-2" element={<BaseTemplate2 />} />
+      </Routes>
+      {/* <Footer /> */}
+    </div>
   );
 };
 
