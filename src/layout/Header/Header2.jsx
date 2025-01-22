dddb7bdc193c82cff42edbddbf0bff3b1e096e9e
manisha-digihost <manisha.prajapati@digihost.in>
2025-01-22 12:53:32 +0530

    folder structure

--- src/layout/Header/Header2.jsx
+++ src/layout/Header/Header2.jsx
@@ -4,54 +4,106 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
+import {
+  FaPhone,
+  FaEnvelope,
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
 
-const Header1 = () => {
+const Header2 = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary fixed-top py-3">
-      <Container>
-        <Navbar.Brand href="#home" className="fw-bold fs-3">
-          <span className="text-primary">Best</span> Rate
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
-            <Link to="#home" className="mx-2 nav-link">
-              Home
-            </Link>
-            <Link to="#about" className="mx-2 nav-link">
-              About
-            </Link>
-            <NavDropdown
-              title="Services"
-              id="basic-nav-dropdown"
-              className="mx-2"
-            >
-              <NavDropdown.Item href="#products">Products</NavDropdown.Item>
-              <NavDropdown.Item href="#testimonials">
-                Testimonials
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#gallery">Gallery</NavDropdown.Item>
-            </NavDropdown>
-            <Link to="#contact" className="mx-2 nav-link">
-              Contact
-            </Link>
-            {/* <Link to="/">Template</Link> */}
-            <Link to="/" className="mx-2 nav-link">
-              Template1
-            </Link>
-          </Nav>
-          <div className="d-flex">
-            {/* <button className="btn btn-primary rounded-pill px-4">
-              Get Started
-            </button> */}
-            <button className="btn btn-primary rounded-pill px-3 hover-scale">
-              Our Services
-            </button>
+    <>
+      {/* Top Mini Header */}
+      <div className="bg-dark py-2">
+        <Container>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex text-white">
+              <div className="me-4">
+                <FaPhone className="me-2" />
+                <small>+1 234 567 890</small>
+              </div>
+              <div>
+                <FaEnvelope className="me-2" />
+                <small>info@bestrate.com</small>
+              </div>
+            </div>
+            <div className="d-flex">
+              <a href="#" className="text-white me-3">
+                <FaFacebookF />
+              </a>
+              <a href="#" className="text-white me-3">
+                <FaTwitter />
+              </a>
+              <a href="#" className="text-white">
+                <FaInstagram />
+              </a>
+            </div>
           </div>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Container>
+      </div>
+
+      {/* Main Header */}
+      <Navbar expand="lg" className="bg-white shadow-sm">
+        <Container>
+          <Navbar.Brand href="#home" className="d-flex align-items-center">
+            {/* <img
+              src="/logo.png"
+              alt="Best Rate Logo"
+              height="40"
+              className="me-2"
+            /> */}
+            <span className="fw-bold fs-4">
+              <span className="text-primary">Best</span> Rate
+            </span>
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="main-nav" />
+
+          <Navbar.Collapse id="main-nav">
+            <Nav className="ms-auto me-4">
+              <Link to="#home" className="nav-link px-3 fw-medium">
+                Home
+              </Link>
+              <Link to="#about" className="nav-link px-3 fw-medium">
+                About
+              </Link>
+              <NavDropdown
+                title="Services"
+                id="services-dropdown"
+                className="px-2"
+              >
+                <NavDropdown.Item href="#products" className="py-2">
+                  <i className="bi bi-box me-2"></i>
+                  Products
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#testimonials" className="py-2">
+                  <i className="bi bi-chat-quote me-2"></i>
+                  Testimonials
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#gallery" className="py-2">
+                  <i className="bi bi-images me-2"></i>
+                  Gallery
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Link to="#contact" className="nav-link px-3 fw-medium">
+                Contact
+              </Link>
+              <Link to="/" className="nav-link px-3 fw-medium">
+                Template1
+              </Link>
+            </Nav>
+
+            {/* <button className="btn btn-primary rounded-pill px-4 py-2">
+              <i className="bi bi-telephone-fill me-2"></i>
+              Get Quote
+            </button> */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
-export default Header1;
+export default Header2;
--- src/pages/Template2.jsx
+++ src/pages/Template2.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Hero from "../components/Template1/Hero";
-import USPS from "../components/Template1/USPS";
-import About from "../components/Template1/About";
-import Products from "../components/Template1/Products";
-import Testimonials from "../components/Template1/Testimonials";
-import Gallery from "../components/Template1/Gallery";
-import Contact from "../components/Template1/Contact";
+import Hero from "../components/Template2/Hero";
+import USPS from "../components/Template2/USPS";
+import About from "../components/Template2/About";
+import Products from "../components/Template2/Products";
+import Testimonials from "../components/Template2/Testimonials";
+import Gallery from "../components/Template2/Gallery";
+import Contact from "../components/Template2/Contact";
 import Header2 from "../layout/Header/Header2";
 import Footer2 from "../layout/Footer/Footer2";
 
