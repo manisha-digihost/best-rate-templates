8aa00761452b9231597e82505267cbc51c547bc0
Devendra Mali <devendra.mali@digihost.in>
2025-01-23 15:21:10 +0530

    new theme

--- src/components/Template3/Gallery.jsx
+++ src/components/Template3/Gallery.jsx
@@ -1,54 +1,124 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
-// Import required lightgallery CSS
+const FilterableGallery = () => {
+  const [activeFilter, setActiveFilter] = useState("All");
 
-import { Container, Row, Col } from "react-bootstrap";
-import Masonry, {
-  ResponsiveMasonry,
-} from "https://cdn.skypack.dev/react-responsive-masonry@2.1.0";
+  const filters = ["All", "People", "Events", "Trips", "Plants"];
 
-const Gallery = () => {
   const images = [
-    "https://cdn.pixabay.com/photo/2020/11/01/19/41/autumn-5704791_1280.jpg",
-    "https://cdn.pixabay.com/photo/2020/07/27/02/30/hands-5441201_1280.jpg",
-    "https://cdn.pixabay.com/photo/2020/09/27/04/15/cat-5605615_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2020/11/08/09/41/deer-5723225_1280.jpg",
-    "https://cdn.pixabay.com/photo/2020/11/15/22/08/person-5747420_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2020/11/08/09/41/deer-5723225_1280.jpg",
-    "https://cdn.pixabay.com/photo/2020/03/14/21/56/wine-4931923_1280.jpg",
-    // "https://cdn.pixabay.com/photo/2020/04/19/12/26/thread-5063401_1280.jpg",
-    // "https://cdn.pixabay.com/photo/2020/07/11/11/34/mam-tor-5393685_1280.jpg",
-    // "https://cdn.pixabay.com/photo/2020/03/14/21/56/wine-4931923_1280.jpg",
-    // "https://cdn.pixabay.com/photo/2020/07/03/14/43/waterdrops-5366584_1280.jpg",
-    // "https://cdn.pixabay.com/photo/2020/09/27/04/15/cat-5605615_960_720.jpg",
+    {
+      author: "The Lazy Artist Gallery",
+      tag: "People",
+      src: "https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/1.jpg?raw=true",
+    },
+    {
+      author: "Daria Shevtsova",
+      tag: "Events",
+      src: "https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/2.jpg?raw=true",
+    },
+    {
+      author: "Kasuma",
+      tag: "Trips",
+      src: "https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/3.jpg?raw=true",
+    },
+    {
+      author: "Dominika Roseclay",
+      tag: "Plants",
+      src: "https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/4.jpg?raw=true",
+    },
+    {
+      author: "Scott Webb",
+      tag: "Plants",
+      src: "https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/5.jpg?raw=true",
+    },
+    {
+      author: "Jeffrey Czum",
+      tag: "People",
+      src: "https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/6.jpg?raw=true",
+    },
+    {
+      author: "Dominika Roseclay",
+      tag: "Events",
+      src: "https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/7.jpg?raw=true",
+    },
+    {
+      author: "Valeria Boltneva",
+      tag: "Trips",
+      src: "https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/8.jpg?raw=true",
+    },
   ];
-  const columnsCountBreakPoints = { 350: 1, 750: 2, 900: 3 };
+
+  // Filter logic
+  const filteredImages =
+    activeFilter === "All"
+      ? images
+      : images.filter((image) => image.tag === activeFilter);
+
+  const handleFilter = (filter) => {
+    setActiveFilter(filter);
+  };
 
   return (
-    <section className="gallery section">
-      <Container>
-        <div className="text-center mb-5">
-          <span className="px-3 py-2 rounded-pill bg-primary text-white mb-2 d-inline-block">
-            Our Gallery
-          </span>
-          <h2 className="display-5 fw-bold mb-3">
-            Solutions That Drive Success
-          </h2>
-          <p className="text-muted lead mx-auto" style={{ maxWidth: "700px" }}>
-            Discover our range of financial services designed to help you
-            achieve your goals and secure your future
-          </p>
-        </div>
-        <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
-          <Masonry gutter={4}>
-            {images.map((image) => (
-              <img src={image} />
-            ))}
-          </Masonry>
-        </ResponsiveMasonry>
-      </Container>
-    </section>
+    <Container className="py-5">
+      <div className="text-center mb-5">
+        <span className="px-3 py-2 rounded-pill bg-primary text-white mb-2 d-inline-block">
+          Our Gallery
+        </span>
+        <h2 className="display-5 fw-bold mb-3">
+          Solutions That Drive Success
+        </h2>
+        <p className="text-muted lead mx-auto" style={{ maxWidth: "700px" }}>
+          Discover our range of financial services designed to help you
+          achieve your goals and secure your future
+        </p>
+      </div>
+      <Row className="text-center mb-4">
+        <Col>
+          {filters.map((filter) => (
+            <Button
+              key={filter}
+              // variant={activeFilter === filter ? "primary" : "outline-primary"}
+              className={activeFilter === filter ? "gallery-btn active mx-2" : "gallery-btn mx-2"}
+              onClick={() => handleFilter(filter)}
+            >
+              {filter}
+            </Button>
+          ))}
+        </Col>
+      </Row>
+
+      <Row>
+        {filteredImages.map((image, index) => (
+          <Col key={index} md={3} className="mb-4">
+            <div className="gallery-item">
+              <img
+                src={image.src}
+                alt={image.author}
+                className="img-fluid rounded"
+              />
+              <div className="mt-2 text-center">
+                <h6>{image.author}</h6>
+              
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+      <style jsx>{`
+     .gallery-btn{
+        background-color: transparent !important;
+        color:#000 ;
+        margin-bottom:10px;
+
+     }
+     .gallery-btn.active,.gallery-btn:hover{
+         background-color: #0077be !important;
+         color;#fff !important;
+     }
+     `}</style>
+    </Container>
   );
 };
 
-export default Gallery;
+export default FilterableGallery;
--- src/layout/Footer/Footer3.jsx
+++ src/layout/Footer/Footer3.jsx
@@ -4,17 +4,43 @@ import { FaXTwitter } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
 import { IoCall } from "react-icons/io5";
 import { Container } from "react-bootstrap";
+import { IoLocationSharp } from "react-icons/io5";
 
 const Footer1 = () => {
   return (
-    <footer className="bg-dark text-white py-5 pb-3">
+    <footer className="bg-dark text-white py-5">
       <Container>
-        <div className="text-center">
+        <div className="cta-3 bg-primary">
+          <div className="row">
+            <div className="col-lg-6">
+              <h3>Contact To Get Updated</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            </div>
+            <div className="col-lg-6 input-box">
+              <input type="text" name="" id="" placeholder="abc@gmail.com"/>
+              <button type="button">Submit</button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
           {/* Company Info */}
-            <h5 className="text-white mb-3">Best Rate Website</h5>
-            
-        
-            <ul className="list-unstyled d-flex justify-content-center mt-5" style={{gap:"20px"}}>
+          <div className="col-lg-4 mb-4">
+            <h1 className="text-white mb-3"><span className="best text-primary">Best Rate</span><br /> Website</h1>
+            <p className="text-white mb-4">
+              Providing quality digital solutions and exceptional service to help your business grow online.
+            </p>
+            <div className="social-links">
+              <a href="#" className="text-white me-3"><FaFacebookF /></a>
+              <a href="#" className="text-white me-3"><FaXTwitter /></a>
+              <a href="#" className="text-white me-3"><FaInstagram /></a>
+              <a href="#" className="text-white me-3"><FaLinkedinIn /></a>
+            </div>
+          </div>
+
+          {/* Quick Links */}
+          <div className="col-lg-4 mb-4">
+            <h5 className="text-white mb-3">Quick Links</h5>
+            <ul className="list-unstyled">
               <li className="mb-2">
                 <a href="#!" className="text-white text-decoration-none">Home</a>
               </li>
@@ -28,16 +54,10 @@ const Footer1 = () => {
                 <a href="#!" className="text-white text-decoration-none">Contact</a>
               </li>
             </ul>
-            <div className="social-links mt-5">
-              <a href="#" className="text-white me-3"><FaFacebookF /></a>
-              <a href="#" className="text-white me-3"><FaXTwitter /></a>
-              <a href="#" className="text-white me-3"><FaInstagram /></a>
-              <a href="#" className="text-white me-3"><FaLinkedinIn /></a>
-            </div>
-      
+          </div>
 
           {/* Contact Info */}
-          {/* <div className="col-lg-4 mb-4">
+          <div className="col-lg-4 mb-4">
             <h5 className="text-white mb-3">Contact Us</h5>
             <div className="d-flex align-items-center mb-3 text-white">
               <IoMdMail className="me-2" />
@@ -47,21 +67,73 @@ const Footer1 = () => {
               <IoCall className="me-2" />
               <span>+1 (555) 123-4567</span>
             </div>
-            <address className="text-white mb-0">
-              123 Business Street<br />
+            <address className="text-white mb-0 d-flex">
+            <IoLocationSharp className="me-2"  />
+            <p className="text-white">123 Business Street<br />
               New York, NY 10001<br />
-              United States
+              United States</p>
+            
             </address>
-          </div> */}
+          </div>
         </div>
 
         {/* Copyright */}
-      </Container>
         <div className="border-top border-secondary pt-4 mt-4 text-center">
           <p className="text-white mb-0">
             © {new Date().getFullYear()} Best Rate Website. All rights reserved.
           </p>
         </div>
+      </Container>
+      <style jsx>{`
+        .cta-3{
+            padding:25px;
+            // background-color: #0077be;
+            margin-bottom:45px;
+            border-radius:20px
+          }
+          .cta-3 .row{
+            gap:10px 0
+          }
+          .cta-3 h3{
+            margin-bottom:10px;
+            font-weight:700
+          }
+          .cta-3 p{
+            color:#fff
+          }
+          .cta-3 .input-box{
+            display:flex;
+            gap:10px;
+            align-items: center;
+          }
+          .cta-3 .input-box input{
+            width:60%;
+            padding:8px 10px;
+            border-radius:10px;
+            background-color:#fff !important;
+            box-shadow:none;
+            outline:none;
+            border:none;
+          }
+          .cta-3 .input-box button{
+            width:30%;
+            padding:8px 5px;
+            border-radius: 10px;
+            background-color:#fff ;
+            box-shadow:none;
+            outline:none;
+            border:1px solid #0077be;
+            transition: all .3s linear
+          }
+            .cta-3 .input-box button:hover{
+            background-color: #0077be !important;
+            box-shadow:0px 0px 13px #fff;
+            color:#fff;
+          }
+          .best{
+            font-weight: 700;
+          }
+     `}</style>
     </footer>
   );
 };
